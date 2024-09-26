6a821d8930f5723691e4fe30308700a1014c6c1d
Abhikakadiya2301 <abhihkakadiya2301@gmail.com>
2024-09-26 16:50:49 -0400

    Commiting before making connection

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, request, render_template
 from google.cloud import storage
-import os
+import os,pyodbc
 
 app = Flask(__name__)
+
+
+connection_string = (
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    f"SERVER={os.getenv('DB_HOST')};"
+    f"DATABASE={os.getenv('DB_NAME')};"
+    f"UID={os.getenv('DB_USER')};"
+    f"PWD={os.getenv('DB_PASSWORD')};"
+    "Trusted_Connection=no;"
+    "Encrypt=yes;"
+)
+
+conn = pyodbc.connect(connection_string)
 
 # Configure this to your GCP project and bucket
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'abiding-ion-436022-b5-e533269739bc.json'
@@ -33,5 +46,5 @@ def upload_file():
             return f'File {file.filename} uploaded to {BUCKET_NAME}.'
     return render_template('upload.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0', port=5000)
