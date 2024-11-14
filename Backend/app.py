7dcaae06f2d9e39118458436c1d926be2b48cb69
Abhikakadiya2301 <abhihkakadiya2301@gmail.com>
2024-11-14 08:46:49 -0500

    Development complete

--- Backend/app.py
+++ Backend/app.py
@@ -10,7 +10,7 @@ from pubsub_utils import publish_message, subscribe_messages
 
 load_dotenv()
 
-app = Flask(__name__,template_folder = "../Frontend/static",static_folder="../Frontend/static")
+app = Flask(__name__,template_folder = "../Frontend",static_folder="../Frontend/static")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
